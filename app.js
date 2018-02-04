a6bbeda53631f54f913464c1c51180a962c94725
Simon Dürr <simon_duerr@hotmail.com>
2018-02-04 14:25:55 +0100

    Display buildings

--- app.js
+++ app.js
@@ -1,49 +1,77 @@
 /// <reference path="phaser.d.ts"/>
+/// <reference path="phaser.plugin.isometric.d.ts"/>
+var IsoSprite = Phaser.Plugin.Isometric.IsoSprite;
 var BasicGame = /** @class */ (function () {
     function BasicGame() {
         var _this = this;
         this.tiles = [[], [], [], [], [], []];
+        this.builtObjects = {};
+        this.clickableObjects = [];
         this.preload = function () {
             _this.game.load.image('tile', 'tile.png');
+            _this.game.load.image('house1', 'tile-house.png');
             _this.game.time.advancedTiming = true;
             _this.game.plugins.add(new Phaser.Plugin.Isometric(_this.game));
             // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
             // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
             _this.game.iso.anchor.setTo(0.5, 0.2);
+            _this.game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
         };
         this.create = function () {
             // Create a group for our tiles.
             _this.isoGroup = _this.game.add.group();
-            // Let's make a load of tiles on a grid.
+            _this.clickableGroup = _this.game.add.group();
             _this.spawnTiles();
             // Provide a 3D position for the cursor
             _this.cursorPos = new Phaser.Plugin.Isometric.Point3();
+            // this.clickableGroup.enableBody = true;
+            // this.clickableGroup.physicsBodyType = Phaser.Plugin.Isometric.ISOARCADE;
         };
         this.update = function () {
-            if (_this.game.input.activePointer.leftButton.isDown) {
+            if (_this.game.input.activePointer.leftButton.justReleased(20)) {
+                //TODO: 1. check buttons
+                //2. check builtObjects
                 // Update the cursor position.
                 // It's important to understand that screen-to-isometric projection means you have to specify a z position manually, as this cannot be easily
                 // determined from the 2D pointer position without extra trickery. By default, the z position is 0 if not set.
                 _this.game.iso.unproject(_this.game.input.activePointer.position, _this.cursorPos);
-                // Loop through all tiles and test to see if the 3D position from above intersects with the automatically generated IsoSprite tile bounds.
-                _this.isoGroup.forEach(function (tile) {
-                    var inBounds = tile.isoBounds.containsXY(_this.cursorPos.x, _this.cursorPos.y);
-                    // If it does, do a little animation and tint change.
-                    if (!tile.selected && inBounds) {
-                        tile.selected = true;
-                        _this.onDown(tile);
-                        _this.game.add.tween(tile).to({ isoZ: 4 }, 200, Phaser.Easing.Quadratic.InOut, true);
-                    }
-                    else if (tile.selected && !inBounds) {
-                        tile.selected = false;
-                        _this.game.add.tween(tile).to({ isoZ: 0 }, 200, Phaser.Easing.Quadratic.InOut, true);
+                var selectedObject_1 = null;
+                _this.clickableGroup.forEach(function (builtObject) {
+                    var inBounds = builtObject.isoBounds.containsXY(_this.cursorPos.x, _this.cursorPos.y);
+                    if (!selectedObject_1 && !builtObject.selected && inBounds) {
+                        builtObject.selected = true;
+                        _this.game.add.tween(builtObject).to({ isoZ: 4 }, 200, Phaser.Easing.Quadratic.InOut, true);
+                        selectedObject_1 = builtObject;
+                        console.log(builtObject.isoBounds);
+                        console.log(_this.cursorPos);
+                        console.log(selectedObject_1.customId);
                     }
                 });
+                if (!selectedObject_1) {
+                    //3. check tiles
+                    // Loop through all tiles and test to see if the 3D position from above intersects with the automatically generated IsoSprite tile bounds.
+                    _this.isoGroup.forEach(function (tile) {
+                        var inBounds = tile.isoBounds.containsXY(_this.cursorPos.x, _this.cursorPos.y);
+                        // If it does, do a little animation and tint change.
+                        if (!tile.selected && inBounds) {
+                            tile.selected = true;
+                            _this.onDown(tile);
+                            _this.game.add.tween(tile).to({ isoZ: 4 }, 200, Phaser.Easing.Quadratic.InOut, true);
+                        }
+                        else if (tile.selected && !inBounds) {
+                            tile.selected = false;
+                            _this.game.add.tween(tile).to({ isoZ: 0 }, 200, Phaser.Easing.Quadratic.InOut, true);
+                        }
+                    });
+                }
             }
         };
         this.render = function () {
             _this.game.debug.text("Move your mouse around!", 2, 36, "#ffffff");
             _this.game.debug.text(_this.game.time.fps || '--', 2, 14, "#a7aebe");
+            _this.clickableGroup.forEach(function (tile) {
+                _this.game.debug.body(tile, "#00dd00", false);
+            });
         };
         this.spawnTiles = function () {
             $.ajax({
@@ -56,12 +84,21 @@ var BasicGame = /** @class */ (function () {
                         0x555555,
                         0x5555ff //water
                     ];
-                    $.each(data, function (i, element) {
+                    var buildingSprites = {
+                        1: 'house1',
+                        2: 'house1'
+                    };
+                    var buildingColors = {
+                        1: 0xff9999,
+                        2: 0xff2222
+                    };
+                    console.log(data);
+                    $.each(data.builtObjects, function (i, element) {
+                        _this.builtObjects[element.id] = element;
+                    });
+                    $.each(data.tiles, function (i, element) {
                         _this.tiles[element.y][element.x] = element;
                     });
-                    var tiles_x = 5;
-                    var tiles_y = 5;
-                    var tiles_w = 36.57;
                     _this.tiles.forEach(function (row, yy) {
                         row.forEach(function (element, xx) {
                             var tile = _this.game.add.isoSprite(xx * 38, yy * 38, 0, 'tile', 0, _this.isoGroup);
@@ -69,6 +106,17 @@ var BasicGame = /** @class */ (function () {
                             tile.anchor.set(0.5, 0);
                             tile.inputEnabled = true;
                             tile.customId = element.id;
+                            tile.builtObject = element.builtObject;
+                            if (element.builtObject) {
+                                console.log(element.builtObject);
+                                var building = _this.builtObjects[element.builtObject];
+                                var builtObject = _this.game.add.isoSprite(xx * 38, yy * 38, 0, buildingSprites[building.object], 0, _this.clickableGroup);
+                                builtObject.anchor.set(0.5, 0.5);
+                                builtObject.tint = buildingColors[building.object];
+                                builtObject.customId = element.builtObject;
+                                builtObject.inputEnabled = true;
+                                _this.clickableObjects.push(builtObject);
+                            }
                         });
                     });
                 }
--- app.ts
+++ app.ts
@@ -1,8 +1,14 @@
 /// <reference path="phaser.d.ts"/>
+/// <reference path="phaser.plugin.isometric.d.ts"/>
+import IsoSprite = Phaser.Plugin.Isometric.IsoSprite;
+
 class BasicGame {
     game: Phaser.Game;
     tiles = [[], [], [], [], [], []];
+    builtObjects: { [id: number]: builtObject; } = {};
+    clickableObjects: clickableInterface[] = [];
     isoGroup;
+    clickableGroup;
     cursorPos;
     cursor;
 
@@ -17,6 +23,7 @@ class BasicGame {
 
     preload = () => {
         this.game.load.image('tile', 'tile.png');
+        this.game.load.image('house1', 'tile-house.png');
 
         this.game.time.advancedTiming = true;
         this.game.plugins.add(new Phaser.Plugin.Isometric(this.game));
@@ -24,46 +31,73 @@ class BasicGame {
         // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
         // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
         this.game.iso.anchor.setTo(0.5, 0.2);
+        this.game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
+
 
     };
     create = () => {
         // Create a group for our tiles.
         this.isoGroup = this.game.add.group();
-
-        // Let's make a load of tiles on a grid.
+        this.clickableGroup = this.game.add.group();
         this.spawnTiles();
 
         // Provide a 3D position for the cursor
         this.cursorPos = new Phaser.Plugin.Isometric.Point3();
+
+        // this.clickableGroup.enableBody = true;
+        // this.clickableGroup.physicsBodyType = Phaser.Plugin.Isometric.ISOARCADE;
+
     };
     update = () => {
-        if(this.game.input.activePointer.leftButton.isDown)
-        {
+        if (this.game.input.activePointer.leftButton.justReleased(20)) {
+            //TODO: 1. check buttons
+
+            //2. check builtObjects
+
             // Update the cursor position.
             // It's important to understand that screen-to-isometric projection means you have to specify a z position manually, as this cannot be easily
             // determined from the 2D pointer position without extra trickery. By default, the z position is 0 if not set.
             this.game.iso.unproject(this.game.input.activePointer.position, this.cursorPos);
 
-            // Loop through all tiles and test to see if the 3D position from above intersects with the automatically generated IsoSprite tile bounds.
-            this.isoGroup.forEach((tile) => {
-                let inBounds = tile.isoBounds.containsXY(this.cursorPos.x, this.cursorPos.y);
-                // If it does, do a little animation and tint change.
-                if (!tile.selected && inBounds) {
-                    tile.selected = true;
-                    this.onDown(tile);
-                    this.game.add.tween(tile).to({isoZ: 4}, 200, Phaser.Easing.Quadratic.InOut, true);
-                }
-                // If not, revert back to how it was.
-                else if (tile.selected && !inBounds) {
-                    tile.selected = false;
-                    this.game.add.tween(tile).to({isoZ: 0}, 200, Phaser.Easing.Quadratic.InOut, true);
+            let selectedObject = null;
+            this.clickableGroup.forEach((builtObject) => {
+                let inBounds = builtObject.isoBounds.containsXY(this.cursorPos.x, this.cursorPos.y);
+                if (!selectedObject && !builtObject.selected && inBounds) {
+                    builtObject.selected = true;
+                    this.game.add.tween(builtObject).to({isoZ: 4}, 200, Phaser.Easing.Quadratic.InOut, true);
+                    selectedObject = builtObject;
+                    console.log(builtObject.isoBounds);
+                    console.log(this.cursorPos);
+                    console.log(selectedObject.customId);
                 }
             });
+
+            if (!selectedObject) {
+                //3. check tiles
+                // Loop through all tiles and test to see if the 3D position from above intersects with the automatically generated IsoSprite tile bounds.
+                this.isoGroup.forEach((tile) => {
+                    let inBounds = tile.isoBounds.containsXY(this.cursorPos.x, this.cursorPos.y);
+                    // If it does, do a little animation and tint change.
+                    if (!tile.selected && inBounds) {
+                        tile.selected = true;
+                        this.onDown(tile);
+                        this.game.add.tween(tile).to({isoZ: 4}, 200, Phaser.Easing.Quadratic.InOut, true);
+                    }
+                    // If not, revert back to how it was.
+                    else if (tile.selected && !inBounds) {
+                        tile.selected = false;
+                        this.game.add.tween(tile).to({isoZ: 0}, 200, Phaser.Easing.Quadratic.InOut, true);
+                    }
+                });
+            }
         }
     };
     render = () => {
         this.game.debug.text("Move your mouse around!", 2, 36, "#ffffff");
         this.game.debug.text(this.game.time.fps || '--', 2, 14, "#a7aebe");
+        this.clickableGroup.forEach((tile) => {
+            this.game.debug.body(tile, "#00dd00", false);
+        });
     };
     spawnTiles = () => {
         $.ajax({
@@ -76,13 +110,21 @@ class BasicGame {
                     0x555555, //rock
                     0x5555ff  //water
                 ];
-
-                $.each(data, (i, element) => {
+                let buildingSprites: { [id: number]: string; } = {
+                    1: 'house1',
+                    2: 'house1'
+                };
+                let buildingColors: { [id: number]: number; } = {
+                    1: 0xff9999,
+                    2: 0xff2222
+                };
+                console.log(data);
+                $.each(data.builtObjects, (i, element) => {
+                    this.builtObjects[element.id] = element;
+                });
+                $.each(data.tiles, (i, element) => {
                     this.tiles[element.y][element.x] = element;
                 });
-                let tiles_x = 5;
-                let tiles_y = 5;
-                let tiles_w = 36.57;
                 this.tiles.forEach((row, yy) => {
                     row.forEach((element, xx) => {
                         let tile = this.game.add.isoSprite(xx * 38, yy * 38, 0, 'tile', 0, this.isoGroup);
@@ -90,6 +132,18 @@ class BasicGame {
                         tile.anchor.set(0.5, 0);
                         tile.inputEnabled = true;
                         tile.customId = element.id;
+                        tile.builtObject = element.builtObject;
+
+                        if (element.builtObject) {
+                            console.log(element.builtObject);
+                            let building = this.builtObjects[element.builtObject];
+                            let builtObject = this.game.add.isoSprite(xx * 38, yy * 38, 0, buildingSprites[building.object], 0, this.clickableGroup);
+                            builtObject.anchor.set(0.5, 0.5);
+                            builtObject.tint = buildingColors[building.object];
+                            builtObject.customId = element.builtObject;
+                            builtObject.inputEnabled = true;
+                            this.clickableObjects.push(builtObject);
+                        }
                     });
                 });
             }
@@ -101,6 +155,20 @@ class BasicGame {
         console.log('onDown');
         console.log(sprite.customId);
     }
+}
+
+interface builtObject extends clickableInterface {
+    id: number;
+    selected: boolean;
+
+    level: number;
+    type: number;
+    object: number;
+}
+
+interface clickableInterface extends IsoSprite {
+    id: number;
+    selected: boolean;
 }
 
 
